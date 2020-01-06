94c6c4043739877cd4086ef6bc76b854d1bf5c9b
Lee Johnston <lee.johnston@ni.com>
2020-01-05 22:46:06 -0600

    TST: use object-oriented api in test_mplinterface

--- tests/test_mplinterface.py
+++ tests/test_mplinterface.py
@@ -8,7 +8,8 @@ from unit_system.predefined_units import A, V, m, s, Hz, Quantity
 def test_label():
     x = [1, 2, 3] * A
     y = [4, 5, 6] * V
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y)
     assert ax.xaxis.have_units()
     expected = "$I\\;/\\;{\\rm A}$"
@@ -21,7 +22,8 @@ def test_label():
 def test_labelwithpar():
     x = [1, 2, 3] * s
     y = [4, 5, 6] * m / s
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y)
     assert ax.xaxis.have_units()
     expected = "$t\\;/\\;{\\rm s}$"
@@ -34,12 +36,13 @@ def test_labelwithpar():
 def test_labelwithpwr():
     x = [1, 2, 3] * s
     y = [4, 5, 6] * m ** 2 / s ** 2
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y)
     expected = "$q\\;/\\;{\\rm (m^2/s^2)}$"
     assert ax.yaxis.get_label().get_text() == expected
     y.to("(m/s)**2")
-    _, ax = plt.subplots()
+    ax.clear()
     ax.plot(x, y)
     expected = "$q\\;/\\;{\\rm (m/s)^2}$"
     assert ax.yaxis.get_label().get_text() == expected
@@ -50,7 +53,8 @@ def test_qsym():
     x.qsym = "t_r"
     y = [4, 5, 6] * (m / s) ** 2
     y.qsym = "v_max**2"
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y)
     assert ax.xaxis.have_units()
     expected = "$t_{\\rm r}\\;/\\;{\\rm s}$"
@@ -59,7 +63,7 @@ def test_qsym():
     expected = "$v_{\\rm max}^2\\;/\\;{\\rm (m^2/s^2)}$"
     assert ax.yaxis.get_label().get_text() == expected
     y.qsym = r"$v_{\rm max}^2$"
-    _, ax = plt.subplots()
+    ax.clear()
     ax.plot(x, y)
     expected = "$v_{\\rm max}^2\\;/\\;{\\rm (m^2/s^2)}$"
     assert ax.yaxis.get_label().get_text() == expected
@@ -70,7 +74,8 @@ def test_std_qsym():
     x.to("Hz")
     dB = Quantity(1, "dB")
     y = [4, 5, 6] * dB
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y)
     expected = "$f\\;/\\;{\\rm Hz}$"
     assert ax.xaxis.get_label().get_text() == expected
@@ -81,7 +86,8 @@ def test_std_qsym():
 def test_set_units():
     x = [0, 1, 2] * s
     y = [1, 2, 3] * m
-    _, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     ax.plot(x, y, xunits="ms")
     results = ax.lines[0].get_data()[0] == Quantity([0, 1000, 2000], "ms", "ms")
     assert results.all()
@@ -91,4 +97,6 @@ def test_set_wrong_units():
     with raises(ConversionError):
         x = [0, 1, 2] * s
         y = [1, 2, 3] * m
-        plt.plot(x, y, xunits="mm")
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.plot(x, y, xunits="mm")
